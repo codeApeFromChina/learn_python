61d47fd7fe126e88610a028e37a2e035d8813bcc
xinghen <xinghen.1991@gmail.com>
2017-07-19 19:48:44 +0800

    init

---
+++ python_change/pc2.py
@@ -0,0 +1,20 @@
+
+str = 'g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'
+map = {
+    'm':'k',
+    'q':'o',
+    'g':'e'
+}
+txt = ""
+for c in str:
+    # if c in map :
+    #     txt += map[c]
+    #     print (map[c])
+    # else :
+    #     txt += c
+    #     print (c)
+    print (chr(ord(c) + 2))
+    txt += (chr(ord(c) + 2))
+
+print (txt)
+print (str)
