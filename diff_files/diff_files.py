8d5dadddc82c0dfd945eb01068136fd292863e4a
xinghen <xinghen.1991@gmail.com>
2018-01-31 16:43:06 +0800

    test

---
+++ diff_files/diff_files.py
@@ -0,0 +1,54 @@
+# codding:utf-8
+import os
+
+
+class DiffFiles(object):
+
+    def __init__(self):
+        # self._root = root
+        self._files = {}
+        self._file_suffix = ['avi', 'mp4', 'wnv', 'flv', 'rmvb', 'rm']
+        # self.f_suffix = set()
+
+    @property
+    def files(self):
+        return self._files
+
+    # @property
+    # def root(self):
+    #     return self._root
+    #
+    # @root.setter
+    # def root(self, path):
+    #     self._root = path
+
+    def travel(self, file_path):
+        for parent, dirs, files in os.walk(file_path):
+
+            if dirs:
+                for file_dir in dirs:
+                    # print("current dirs is : " + parent + '/' + d)
+                    self.travel(parent + '\\' + file_dir)
+            else:
+                for f in files:
+                    # print(os.path.splitext(f)[1][1:])
+                    if os.path.splitext(f)[1][1:] in self._file_suffix:
+                        # print("current files is : " + f)
+                        if not self._files.get(f):
+                            self._files[f] = []
+                        if parent + '\\' + f not in self._files[f]:
+                            self._files[f].append(parent + '\\' + f)
+
+    def travel_multiple_path(self):
+
+
+
+if __name__ == '__main__':
+    path = "G:\\Downloads"
+    d = DiffFiles()
+    d.travel(path)
+
+    for key in sorted(d.files.keys()):
+        if len(d.files.get(key)) > 1:
+            print('file: ' + key + ", path : " + str(d.files.get(key)))
+
